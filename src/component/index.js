78f1e157fa2b6757530e548814b21993d9ceaedf
huawei <huawei.lonny@gmail.com>
2018-12-20 12:39:57 +0800

    edit ..

--- src/component/index.js
+++ src/component/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-// import './index.css';
+import './index.less';
 import { Menu, Icon, Button } from 'antd';
 
 const SubMenu = Menu.SubMenu;
